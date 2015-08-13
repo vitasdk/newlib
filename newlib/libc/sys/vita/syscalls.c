0681b290b6f905054b7c3c2d67d4e36da51c3f12
Ilya Zhuravlev <whatever@xyz.is>
2015-08-13 13:44:33 +0300

    add 'vita' system

---
+++ newlib/libc/sys/vita/syscalls.c
@@ -0,0 +1,127 @@
+#include <errno.h>
+#include <reent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+
+_ssize_t
+_write_r(struct _reent * reent, int fd, const void *buf, size_t nbytes)
+{
+	reent->_errno = ENOSYS;
+	return -1;
+}
+
+void
+_exit(int rc)
+{
+	while (1) {}
+}
+
+int
+_close_r(struct _reent *reent, int fildes)
+{
+	reent->_errno = ENOSYS;
+	return -1;
+}
+
+char *__env[1] = { 0 };
+char **environ = __env;
+
+int
+_execve_r(struct _reent *reent, const char *name, char * const *argv,
+		char * const *env)
+{
+	reent->_errno = ENOSYS;
+	return -1;
+}
+
+int
+_fork_r(struct _reent *reent)
+{
+	errno = ENOSYS;
+	return -1;
+}
+
+int
+_fstat_r(struct _reent *reent, int fildes, struct stat *st)
+{
+	reent->_errno = ENOSYS;
+	return -1;
+}
+
+int
+_getpid_r(struct _reent *reent)
+{
+	reent->_errno = ENOSYS;
+	return -1;
+}
+
+int
+_gettimeofday(struct _reent *reent, struct timeval  *ptimeval, void *ptimezone)
+{
+	reent->_errno = ENOSYS;
+	return -1;
+}
+
+int
+_isatty_r(struct _reent *reent, int file)
+{
+	reent->_errno = ENOSYS;
+	return 0;
+}
+
+int
+_kill_r(struct _reent *reent, int pid, int sig)
+{
+	reent->_errno = ENOSYS;
+	return -1;
+}
+
+int
+_link_r(struct _reent *reent, const char *existing, const char *new)
+{
+	reent->_errno = ENOSYS;
+	return -1;
+}
+
+_off_t
+_lseek_r(struct _reent *reent, int file, _off_t ptr, int dir)
+{
+	errno = ENOSYS;
+	return -1;
+}
+
+int
+_open(struct _reent *reent, char *file, int flags, int mode)
+{
+	reent->_errno = ENOSYS;
+	return -1;
+}
+
+_ssize_t
+_read_r(struct _reent *reent, int file, void *ptr, size_t len)
+{
+	reent->_errno = ENOSYS;
+	return -1;
+}
+
+int
+_readlink_r(struct _reent *reent, const char *path, char *buf, size_t bufsize)
+{
+	reent->_errno = ENOSYS;
+	return -1;
+}
+
+int
+_stat_r(struct _reent *reent, const char *path, struct stat *buf)
+{
+	reent->_errno = EIO;
+	return -1;
+}
+
+int
+_unlink_r(struct _reent *reent, const char * path)
+{
+	reent->_errno = EIO;
+	return (-1);
+}
