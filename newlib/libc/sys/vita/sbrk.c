c7ae9b785da7c4bfbdfcd1be1b29dddf22fd35cc
Ilya Zhuravlev <whatever@xyz.is>
2015-08-13 23:48:52 +0300

    [Vita] Implement a proper thread safe _sbrk_r.

---
+++ newlib/libc/sys/vita/sbrk.c
@@ -0,0 +1,22 @@
+#include <errno.h>
+#include <reent.h>
+
+int _newlib_heap_memblock;
+unsigned _newlib_heap_size;
+char *_newlib_heap_base, *_newlib_heap_end, *_newlib_heap_cur;
+
+char _newlib_sbrk_mutex[32] __attribute__ ((aligned (8)));
+
+void * _sbrk_r(struct _reent *reent, ptrdiff_t incr) {
+	if (sceKernelLockLwMutex(_newlib_sbrk_mutex, 1, 0) < 0 || !_newlib_heap_base || _newlib_heap_cur + incr >= _newlib_heap_end) {
+		sceKernelUnlockLwMutex(_newlib_sbrk_mutex, 1);
+		reent->_errno = ENOMEM;
+		return -1;
+	}
+
+	char *prev_heap_end = _newlib_heap_cur;
+	_newlib_heap_cur += incr;
+
+	sceKernelUnlockLwMutex(_newlib_sbrk_mutex, 1);
+	return (void*) prev_heap_end;
+}
