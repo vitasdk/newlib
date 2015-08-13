624ead333fa22a1e7350ef2d596df180bfbb7aba
Ilya Zhuravlev <whatever@xyz.is>
2015-08-13 23:48:52 +0300

    [Vita] Implement a proper thread safe _sbrk_r.

--- newlib/libc/sys/vita/crt0.c
+++ newlib/libc/sys/vita/crt0.c
@@ -3,16 +3,19 @@ int main(int argc, const char* argv[]);
 extern int _newlib_heap_memblock;
 extern unsigned _newlib_heap_size;
 extern char *_newlib_heap_base, *_newlib_heap_end, *_newlib_heap_cur;
+extern char _newlib_sbrk_mutex[32];
 
 void _init_vita_heap(void) {
-	int ret;
+	// Create a mutex to use inside _sbrk_r
+	if (sceKernelCreateLwMutex(_newlib_sbrk_mutex, "sbrk mutex", 0, 0, 0) < 0)
+		goto failure;
 	// Create a memblock for the heap memory, 32MB
 	_newlib_heap_size = 32 * 1024 * 1024;
 	_newlib_heap_memblock = sceKernelAllocMemBlock("Newlib heap", 0x0c20d060, _newlib_heap_size, 0);
 	if (_newlib_heap_memblock < 0) {
 		goto failure;
 	}
-	if ((ret = sceKernelGetMemBlockBase(_newlib_heap_memblock, &_newlib_heap_base)) < 0) {
+	if (sceKernelGetMemBlockBase(_newlib_heap_memblock, &_newlib_heap_base) < 0) {
 		goto failure;
 	}
 	_newlib_heap_end = _newlib_heap_base + _newlib_heap_size;
