54bab812bbf97b3a5fba9393b6f5bfea8c668287
Ilya Zhuravlev <whatever@xyz.is>
2015-08-15 00:25:01 +0300

    [Vita] Implement most of file related syscalls.

--- newlib/libc/sys/vita/crt0.c
+++ newlib/libc/sys/vita/crt0.c
@@ -1,9 +1,12 @@
+extern char _newlib_fd_mutex[32];
+
 int main(int argc, const char* argv[]);
 
 void _init_vita_newlib(void) {
 	_init_vita_heap();
 	_init_vita_reent();
 	_init_vita_malloc();
+	sceKernelCreateLwMutex(_newlib_fd_mutex, "fd conversion table mutex", 0, 0, 0);
 }
 
 void _start() {
--- newlib/libc/sys/vita/syscalls.c
+++ newlib/libc/sys/vita/syscalls.c
@@ -1,14 +1,36 @@
 #include <errno.h>
+#include <fcntl.h>
 #include <reent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/time.h>
+#include <sys/unistd.h>
+
+#define SCE_ERRNO_MASK 0xFF
+#define MAX_OPEN_FILES 1024
+
+int fd_to_scefd[MAX_OPEN_FILES];
+char _newlib_fd_mutex[32] __attribute__ ((aligned (8)));
 
 _ssize_t
 _write_r(struct _reent * reent, int fd, const void *buf, size_t nbytes)
 {
-	reent->_errno = ENOSYS;
-	return -1;
+	int ret;
+	if ((unsigned)fd > MAX_OPEN_FILES) {
+		reent->_errno = EINVAL;
+		return -1;
+	}
+	if (fd == STDIN_FILENO || fd == STDOUT_FILENO || fd == STDERR_FILENO) {
+		reent->_errno = 0;
+		return nbytes;
+	}
+	ret = sceIoWrite(fd_to_scefd[fd], buf, nbytes);
+	if (ret < 0) {
+		reent->_errno = ret & SCE_ERRNO_MASK;
+		return -1;
+	}
+	reent->_errno = 0;
+	return ret;
 }
 
 void
@@ -18,10 +40,19 @@ _exit(int rc)
 }
 
 int
-_close_r(struct _reent *reent, int fildes)
+_close_r(struct _reent *reent, int fd)
 {
-	reent->_errno = ENOSYS;
-	return -1;
+	if ((unsigned)fd > MAX_OPEN_FILES) {
+		reent->_errno = EINVAL;
+		return -1;
+	}
+	reent->_errno = sceIoClose(fd_to_scefd[fd]) & SCE_ERRNO_MASK;
+	if (reent->_errno == 0) {
+		sceKernelLockLwMutex(_newlib_fd_mutex, 1, 0);
+		fd_to_scefd[fd] = 0;
+		sceKernelUnlockLwMutex(_newlib_fd_mutex, 1);
+	}
+	return 0;
 }
 
 char *__env[1] = { 0 };
@@ -38,12 +69,12 @@ _execve_r(struct _reent *reent, const char *name, char * const *argv,
 int
 _fork_r(struct _reent *reent)
 {
-	errno = ENOSYS;
+	reent->_errno = ENOSYS;
 	return -1;
 }
 
 int
-_fstat_r(struct _reent *reent, int fildes, struct stat *st)
+_fstat_r(struct _reent *reent, int fd, struct stat *st)
 {
 	reent->_errno = ENOSYS;
 	return -1;
@@ -52,8 +83,8 @@ _fstat_r(struct _reent *reent, int fildes, struct stat *st)
 int
 _getpid_r(struct _reent *reent)
 {
-	reent->_errno = ENOSYS;
-	return -1;
+	reent->_errno = 0;
+	return sceKernelGetProcessId();
 }
 
 int
@@ -85,24 +116,89 @@ _link_r(struct _reent *reent, const char *existing, const char *new)
 }
 
 _off_t
-_lseek_r(struct _reent *reent, int file, _off_t ptr, int dir)
+_lseek_r(struct _reent *reent, int fd, _off_t ptr, int dir)
 {
-	errno = ENOSYS;
-	return -1;
+	int ret;
+	if ((unsigned)fd > MAX_OPEN_FILES || fd == STDIN_FILENO || fd == STDOUT_FILENO || fd == STDERR_FILENO) {
+		reent->_errno = EINVAL;
+		return -1;
+	}
+	ret = sceIoLseek32(fd_to_scefd[fd], ptr, dir);
+	if (ret < 0) {
+		reent->_errno = ret & SCE_ERRNO_MASK;
+		return -1;
+	}
+	return ret;
+}
+
+int _fcntl2sony(int flags) {
+	int out = 0;
+	if (flags & O_RDWR)
+		out |= 3;
+	else if (flags & O_WRONLY)
+		out |= 2;
+	else
+		out |= 1;
+	if (flags & O_NONBLOCK)
+		out |= 4;
+	if (flags & O_APPEND)
+		out |= 0x100;
+	if (flags & O_CREAT)
+		out |= 0x200;
+	if (flags & O_TRUNC)
+		out |= 0x400;
+	if (flags & O_EXCL)
+		out |= 0x800;
+	return out;
 }
 
 int
 _open_r(struct _reent *reent, const char *file, int flags, int mode)
 {
-	reent->_errno = ENOSYS;
+	int ret, i, found = 0;
+	flags = _fcntl2sony(flags);
+	ret = sceIoOpen(file, flags, 0600);
+	if (ret < 0) {
+		reent->_errno = ret & SCE_ERRNO_MASK;
+		return -1;
+	}
+	sceKernelLockLwMutex(_newlib_fd_mutex, 1, 0);
+	// skip stdin, stdout, stderr
+	for (i = 3; i < MAX_OPEN_FILES; ++i)
+		if (fd_to_scefd[i] == 0) {
+			found = i;
+			break;
+		}
+	if (!found)
+		goto fail;
+	fd_to_scefd[found] = ret;
+	sceKernelUnlockLwMutex(_newlib_fd_mutex, 1);
+	reent->_errno = 0;
+	return found;
+fail:
+	reent->_errno = EMFILE;
 	return -1;
 }
 
 _ssize_t
-_read_r(struct _reent *reent, int file, void *ptr, size_t len)
+_read_r(struct _reent *reent, int fd, void *ptr, size_t len)
 {
-	reent->_errno = ENOSYS;
-	return -1;
+	int ret;
+	if ((unsigned)fd > MAX_OPEN_FILES) {
+		reent->_errno = EINVAL;
+		return 01;
+	}
+	if (fd == STDIN_FILENO || fd == STDOUT_FILENO || fd == STDERR_FILENO) {
+		reent->_errno = 0;
+		return len;
+	}
+	ret = sceIoRead(fd_to_scefd[fd], ptr, len);
+	if (ret < 0) {
+		reent->_errno = ret & SCE_ERRNO_MASK;
+		return -1;
+	}
+	reent->_errno = 0;
+	return ret;
 }
 
 int
