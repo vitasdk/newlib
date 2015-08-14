5bf33fd616518477d025d51a61b9cc23c96fbbd0
Ilya Zhuravlev <whatever@xyz.is>
2015-08-14 22:55:06 +0300

    [Vita] Implement proper __malloc_lock, __malloc_unlock

---
+++ newlib/libc/sys/vita/mlock.c
@@ -0,0 +1,13 @@
+char _newlib_malloc_mutex[32] __attribute__ ((aligned (8)));
+
+void __malloc_lock(struct _reent *r) {
+	sceKernelLockLwMutex(_newlib_malloc_mutex, 1, 0);
+}
+
+void __malloc_unlock(struct _reent *r) {
+	sceKernelUnlockLwMutex(_newlib_malloc_mutex, 1);
+}
+
+void _init_vita_malloc(void) {
+	sceKernelCreateLwMutex(_newlib_malloc_mutex, "malloc mutex", 2, 0, 0);
+}
