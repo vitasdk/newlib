3d792ab7b89a4685a1ec28dfa503daeafd5621c6
Glenn Anderson <gandersonmail@yahoo.com>
2015-09-04 01:54:32 -0400

    Adds a call to uvl_write for handling STDIN, STDOUT, and STDERR.

--- newlib/libc/sys/vita/syscalls.c
+++ newlib/libc/sys/vita/syscalls.c
@@ -21,6 +21,11 @@ _write_r(struct _reent * reent, int fd, const void *buf, size_t nbytes)
 		return -1;
 	}
 	if (fd == STDIN_FILENO || fd == STDOUT_FILENO || fd == STDERR_FILENO) {
+		ret = uvl_write(fd, buf, nbytes);
+		if (ret < 0) {
+			reent->_errno = ret & SCE_ERRNO_MASK;
+			return -1;
+		}
 		reent->_errno = 0;
 		return nbytes;
 	}
