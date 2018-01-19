e87a363af66c3c628ba9bb62b06b4d9faecfd321
Sunguk Lee <d3m3vilurr@gmail.com>
2018-01-19 21:09:41 +0900

    Change placeholder to vitasdk

--- newlib/libc/sys/vita/select.c
+++ newlib/libc/sys/vita/select.c
@@ -1,6 +1,6 @@
 /*
 
-Copyright (C) 2016, Sunguk Lee
+Copyright (c) 2016, 2017, 2018 vitasdk
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
