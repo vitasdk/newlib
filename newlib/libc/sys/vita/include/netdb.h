f0eacb8c4144c1cd854b8a350c59734bd6c7870a
Sunguk Lee <d3m3vilurr@gmail.com>
2019-08-10 21:35:40 +0900

    Add missing defines for getaddrinfo

--- newlib/libc/sys/vita/include/netdb.h
+++ newlib/libc/sys/vita/include/netdb.h
@@ -63,6 +63,41 @@ struct addrinfo {
 	struct addrinfo *ai_next;
 };
 
+/* Error return codes from getaddrinfo() */
+#define EAI_BADFLAGS        -1      /* Invalid value for `ai_flags' field.  */
+#define EAI_NONAME          -2      /* NAME or SERVICE is unknown.  */
+#define EAI_AGAIN           -3      /* Temporary failure in name resolution.  */
+#define EAI_FAIL            -4      /* Non-recoverable failure in name res.  */
+#define EAI_NODATA          -5      /* No address associated with NAME.  */
+#define EAI_FAMILY          -6      /* `ai_family' not supported.  */
+#define EAI_SOCKTYPE        -7      /* `ai_socktype' not supported.  */
+#define EAI_SERVICE         -8      /* SERVICE not supported for `ai_socktype'.  */
+#define EAI_ADDRFAMILY      -9      /* Address family for NAME not supported.  */
+#define EAI_MEMORY          -10     /* Memory allocation failure.  */
+#define EAI_SYSTEM          -11     /* System error returned in `errno'.  */
+#define EAI_OVERFLOW        -12     /* Argument buffer overflow.  */
+#ifdef __USE_GNU
+#define EAI_INPROGRESS      -100    /* Processing request in progress.  */
+#define EAI_CANCELED        -101    /* Request canceled.  */
+#define EAI_NOTCANCELED     -102    /* Request not canceled.  */
+#define EAI_ALLDONE         -103    /* All requests done.  */
+#define EAI_INTR            -104    /* Interrupted by a signal.  */
+#define EAI_IDN_ENCODE      -105    /* IDN encoding failed.  */
+#endif
+
+/* Flag values for getaddrinfo() */
+#define AI_PASSIVE          0x00000001      /* Get address to use bind() */
+#define AI_CANONNAME        0x00000002      /* Fill ai_canonname */
+#define AI_NUMERICHOST      0x00000004      /* Prevent name resolution */
+#define AI_NUMERICSERV      0x00000008      /* Fon't use name resolution. */
+
+/* Valid flags for addrinfo */
+#define AI_MASK             (AI_PASSIVE | AI_CANONNAME | AI_NUMERICHOST | AI_ADDRCONFIG)
+#define AI_ALL              0x00000100      /* IPv6 and IPv4-mapped (with AI_V4MAPPED) */
+#define AI_V4MAPPED_CFG     0x00000200      /* Accept IPv4-mapped if kernel supports */
+#define AI_ADDRCONFIG       0x00000400      /* Only if any address is assigned */
+#define AI_V4MAPPED         0x00000800      /* Accept IPv4-mapped IPv6 address */
+
 struct hostent *gethostbyname(const char *name);
 struct servent *getservbyname(const char *name, const char *proto);
 int getaddrinfo(const char *node, const char *service, const struct addrinfo *hints, struct addrinfo **res);
