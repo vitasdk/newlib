3e882f20b20197fb15bf66387f639d9a0565308f
Ivan Epifanov <isage.dna@gmail.com>
2021-12-03 18:58:37 +0300

    Store path only for directory descriptors

--- newlib/libc/sys/vita/syscalls.c
+++ newlib/libc/sys/vita/syscalls.c
@@ -289,7 +289,8 @@ _open_r(struct _reent *reent, const char *file, int flags, int mode)
 
 	__vita_fdmap[fd]->sce_uid = ret;
 	__vita_fdmap[fd]->type = is_dir ? VITA_DESCRIPTOR_DIRECTORY : VITA_DESCRIPTOR_FILE;
-	__vita_fdmap[fd]->filename = strdup(full_path);
+	if (is_dir) // needed only for dirs
+		__vita_fdmap[fd]->filename = strdup(full_path);
 
 	free(full_path);
 
