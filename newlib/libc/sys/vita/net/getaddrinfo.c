05a0e4fc957368daa8bf1a29d68302af23dee5dc
Ivan Epifanov <isage.dna@gmail.com>
2022-05-05 21:39:28 +0300

    Implement gai_strerror

--- newlib/libc/sys/vita/net/getaddrinfo.c
+++ newlib/libc/sys/vita/net/getaddrinfo.c
@@ -189,3 +189,36 @@ int getaddrinfo(const char *node, const char *service, const struct addrinfo *hi
 	}
 	return EAI_NODATA;
 }
+
+const char* gai_strerror(int code)
+{
+	switch (code)
+	{
+		case EAI_BADFLAGS:
+			return "Invalid value for ai_flags";
+		case EAI_NONAME:
+			return "hostname nor servname provided, or not known";
+		case EAI_AGAIN:
+			return "Temporary failure in name resolution";
+		case EAI_FAIL:
+			return "Non-recoverable failure in name resolution";
+		case EAI_NODATA:
+			return "No address associated with hostname";
+		case EAI_FAMILY:
+			return "ai_family not supported";
+		case EAI_SOCKTYPE:
+			return "ai_socktype not supported";
+		case EAI_SERVICE:
+			return "servname not supported for ai_socktype";
+		case EAI_ADDRFAMILY:
+			return "Address family for hostname not supported";
+		case EAI_MEMORY:
+			return "Memory allocation failure";
+		case EAI_SYSTEM:
+			return "System error returned in errno";
+		case EAI_OVERFLOW:
+			return "Argument buffer overflow";
+		default:
+			return "Unknown error";
+	}
+}
