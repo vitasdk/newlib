3fb9a5b8c3b1b1730d65923d99473a5458afa55b
Ivan Epifanov <isage.dna@gmail.com>
2022-05-18 13:05:58 +0300

    License

--- newlib/libc/sys/vita/sys/statvfs.h
+++ newlib/libc/sys/vita/sys/statvfs.h
@@ -1,3 +1,27 @@
+/*
+
+Copyright (C) 2022, vitasdk
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+*/
+
 #ifndef _SYS_STATVFS_H
 #define _SYS_STATVFS_H
 
@@ -17,12 +41,12 @@
 struct statvfs {
    unsigned long    f_bsize;
    unsigned long    f_frsize;
-   fsblkcnt_t	    f_blocks;
-   fsblkcnt_t	    f_bfree;
-   fsblkcnt_t	    f_bavail;
-   fsfilcnt_t	    f_files;
-   fsfilcnt_t	    f_ffree;
-   fsfilcnt_t	    f_favail;
+   fsblkcnt_t       f_blocks;
+   fsblkcnt_t       f_bfree;
+   fsblkcnt_t       f_bavail;
+   fsfilcnt_t       f_files;
+   fsfilcnt_t       f_ffree;
+   fsfilcnt_t       f_favail;
    unsigned long    f_fsid;
    unsigned long    f_flag;
    unsigned long    f_namemax;
