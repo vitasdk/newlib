433a85d210cb016e81783cecb5b01678206bb429
SonicMastr <jaylongowie46@gmail.com>
2022-05-29 17:01:41 -0500

    Fix indentation

--- newlib/libc/sys/vita/utime.c
+++ newlib/libc/sys/vita/utime.c
@@ -36,45 +36,45 @@ int
 utimes (const char *filename, const struct timeval times[2])
 {
     struct SceIoStat stat = {0};
-	char* full_path;
-	int rid;
+    char* full_path;
+    int rid;
 
-	full_path = __realpath(filename);
+    full_path = __realpath(filename);
 
-	if(!full_path)
-	{
-		// errno is set by __realpath
-		return -1;
-	}
+    if(!full_path)
+    {
+        // errno is set by __realpath
+        return -1;
+    }
 
-	int ret = sceIoGetstat(full_path, &stat);
-	if (ret < 0)
-	{
-		free(full_path);
-		errno = __vita_sce_errno_to_errno(ret, ERROR_GENERIC);
-		return -1;
-	}
+    int ret = sceIoGetstat(full_path, &stat);
+    if (ret < 0)
+    {
+        free(full_path);
+        errno = __vita_sce_errno_to_errno(ret, ERROR_GENERIC);
+        return -1;
+    }
 
     if (times) {
-		TRY(sceRtcConvertTime_tToDateTime(times[0].tv_sec, &stat.st_atime));
-		TRY(sceRtcConvertTime_tToDateTime(times[1].tv_sec, &stat.st_mtime));
-		stat.st_atime.microsecond = times[0].tv_usec;
-		stat.st_mtime.microsecond = times[1].tv_usec;
-	}
+        TRY(sceRtcConvertTime_tToDateTime(times[0].tv_sec, &stat.st_atime));
+        TRY(sceRtcConvertTime_tToDateTime(times[1].tv_sec, &stat.st_mtime));
+        stat.st_atime.microsecond = times[0].tv_usec;
+        stat.st_mtime.microsecond = times[1].tv_usec;
+    }
     else {
         TRY(sceRtcGetCurrentClockUtc(&stat.st_atime));
         stat.st_mtime = stat.st_atime;
     }
     
-	ret = sceIoChstat(full_path, &stat, SCE_CST_AT | SCE_CST_MT);
-	if (ret < 0)
-	{
-		free(full_path);
-		errno = __vita_sce_errno_to_errno(ret, ERROR_GENERIC);
-		return -1;
-	}
-	free(full_path);
-	return 0;
+    ret = sceIoChstat(full_path, &stat, SCE_CST_AT | SCE_CST_MT);
+    if (ret < 0)
+    {
+        free(full_path);
+        errno = __vita_sce_errno_to_errno(ret, ERROR_GENERIC);
+        return -1;
+    }
+    free(full_path);
+    return 0;
 }
 
 int
@@ -99,70 +99,70 @@ futimens (int fd, const struct timespec times[2])
 {
     struct SceIoStat stat = {0};
     unsigned int bits = 0;
-	int ret = 0;
-	int rid;
+    int ret = 0;
+    int rid;
 
-	DescriptorTranslation *fdmap = __vita_fd_grab(fd);
+    DescriptorTranslation *fdmap = __vita_fd_grab(fd);
 
-	if (!fdmap)
-	{
-		errno = EBADF;
-		return -1;
-	}
+    if (!fdmap)
+    {
+        errno = EBADF;
+        return -1;
+    }
 
-	switch (fdmap->type)
-	{
+    switch (fdmap->type)
+    {
         case VITA_DESCRIPTOR_TTY:
-		case VITA_DESCRIPTOR_FILE:
-		case VITA_DESCRIPTOR_DIRECTORY:
-			ret = sceIoGetstatByFd(fdmap->sce_uid, &stat);
-			break;
-		case VITA_DESCRIPTOR_SOCKET:
-		case VITA_DESCRIPTOR_PIPE:
-			ret = __vita_make_sce_errno(EBADF);
-			break;
-	}
+        case VITA_DESCRIPTOR_FILE:
+        case VITA_DESCRIPTOR_DIRECTORY:
+            ret = sceIoGetstatByFd(fdmap->sce_uid, &stat);
+            break;
+        case VITA_DESCRIPTOR_SOCKET:
+        case VITA_DESCRIPTOR_PIPE:
+            ret = __vita_make_sce_errno(EBADF);
+            break;
+    }
 
-	if (ret < 0)
-	{
-		__vita_fd_drop(fdmap);
-		errno = __vita_sce_errno_to_errno(ret, ERROR_GENERIC);
-		return -1;
-	}
+    if (ret < 0)
+    {
+        __vita_fd_drop(fdmap);
+        errno = __vita_sce_errno_to_errno(ret, ERROR_GENERIC);
+        return -1;
+    }
 
     if (times) {
         SceDateTime dt[2];
         for (int i = 0; i < 2; i++) {
             if ((times[i].tv_nsec != UTIME_NOW) && (times[i].tv_nsec != UTIME_OMIT)) {
-				TRY(sceRtcConvertTime_tToDateTime(times[i].tv_sec, &dt[i]));
-				dt[i].microsecond = times[i].tv_nsec / 1000;
-			} 
-			else {
-				TRY(sceRtcGetCurrentClockUtc(&dt[i]));
-			}
+                TRY(sceRtcConvertTime_tToDateTime(times[i].tv_sec, &dt[i]));
+                dt[i].microsecond = times[i].tv_nsec / 1000;
+            } 
+            else {
+                TRY(sceRtcGetCurrentClockUtc(&dt[i]));
+            }
         }
         stat.st_atime = dt[0];
         stat.st_mtime = dt[1];
-	}
+    }
     else {
         TRY(sceRtcGetCurrentClockUtc(&stat.st_atime));
         stat.st_mtime = stat.st_atime;
     }
 
     if (!times || times[0].tv_nsec != UTIME_OMIT)
-		bits |= SCE_CST_AT;
+        bits |= SCE_CST_AT;
     if (!times || times[1].tv_nsec != UTIME_OMIT)
-		bits |= SCE_CST_MT;
+        bits |= SCE_CST_MT;
 
-	ret = sceIoChstatByFd(fdmap->sce_uid, &stat, bits);
+    ret = sceIoChstatByFd(fdmap->sce_uid, &stat, bits);
 
-	__vita_fd_drop(fdmap);
+    __vita_fd_drop(fdmap);
 
-	if (ret < 0)
-	{
-		errno = __vita_sce_errno_to_errno(ret, ERROR_GENERIC);
-		return -1;
-	}
+    if (ret < 0)
+    {
+        errno = __vita_sce_errno_to_errno(ret, ERROR_GENERIC);
+        return -1;
+    }
 
-	return 0;
+    return 0;
 }
