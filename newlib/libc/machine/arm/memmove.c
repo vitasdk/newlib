38f7ea40db8f648d3a33498fed6e4ea02ca16961
Francisco José García García <frangarcj@gmail.com>
2022-08-20 20:23:28 +0200

    vita: use sceClibMemset and sceClibMemmove

---
+++ newlib/libc/machine/arm/memmove.c
@@ -0,0 +1,8 @@
+#if defined (__vita__)
+# include <psp2/kernel/clib.h>
+void memmove (void *dest, const void *source, size_t n){
+   return sceClibMemmove(dest, source, n);
+}
+#else
+# include "../../string/memmove.c"
+#endif
