a7c6f265ac54981d71b669361751578106d75996
Francisco José García García <frangarcj@gmail.com>
2022-08-24 12:54:34 +0200

    fix: return type for memmove

--- newlib/libc/machine/arm/memmove.c
+++ newlib/libc/machine/arm/memmove.c
@@ -1,6 +1,6 @@
 #if defined (__vita__)
 # include <psp2/kernel/clib.h>
-void memmove (void *dest, const void *source, size_t n){
+void* memmove (void *dest, const void *source, size_t n){
    return sceClibMemmove(dest, source, n);
 }
 #else
