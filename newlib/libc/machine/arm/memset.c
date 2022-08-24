82f5f14408289e3b1f0799fb811f44c1d837deb3
Francisco José García García <frangarcj@gmail.com>
2022-08-24 12:54:55 +0200

    fix: return type for memset

--- newlib/libc/machine/arm/memset.c
+++ newlib/libc/machine/arm/memset.c
@@ -1,6 +1,6 @@
 #if defined (__vita__)
 # include <psp2/kernel/clib.h>
-void memset (void *dest, int c, size_t n){
+void* memset (void *dest, int c, size_t n){
    return sceClibMemset(dest, c, n);
 }
 #else
